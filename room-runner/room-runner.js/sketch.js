0d36fe0cec711b5329846b10bdb2cf5a70f75205
Steve <steven.p.gantz@gmail.com>
2016-02-18 15:19:24 -0500

    Fixed scoping bug and reset resolution values

--- room-runner/room-runner.js/sketch.js
+++ room-runner/room-runner.js/sketch.js
@@ -1,11 +1,31 @@
 function setup() {
-  var width = 600;
+  //create the display
+  var width = 960;
   var height = 600;
   createCanvas(width, height);
-  background(0);
-  noSmooth();
+  background('#222');  
+  
+  //create the "room"
+  var c = color('#FFF'); //defines color 'c'
+  fill(c); //use color variable 'c' as fill color
+  ellipse(width/2, height/2, 600, 600);
+  
+  //declare variables used for circular movement
+  constant = 250;
+  angle = 0.05;
+  scalar = 100;
+  speed = 0.05;
 }
 
 function draw() {
+  //set ellipse placement variables
+  var x = constant + sin(angle) * scalar;
+  var y = constant + cos(angle) * scalar;
   
+  //create the test circle
+  var c = color('#0F0');
+  fill(c);
+  ellipse(x, y, 100, 100);
+  
+  angle = angle + speed;
 }
