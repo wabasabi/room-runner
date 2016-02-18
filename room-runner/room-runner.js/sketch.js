564de06a6f67322486334bb1b08880aeb409fa96
Steve <steven.p.gantz@gmail.com>
2016-02-18 16:01:33 -0500

    Made the ball go along the edge of the circle

--- room-runner/room-runner.js/sketch.js
+++ room-runner/room-runner.js/sketch.js
@@ -1,31 +1,44 @@
 function setup() {
   //create the display
   var width = 960;
+  //var width = 1920;
   var height = 600;
+  //var height = 1200;
   createCanvas(width, height);
   background('#222');  
   
   //create the "room"
   var c = color('#FFF'); //defines color 'c'
   fill(c); //use color variable 'c' as fill color
-  ellipse(width/2, height/2, 600, 600);
+  ellipse(width/2, height/2, height, height);
   
   //declare variables used for circular movement
-  constant = 250;
+  testCircleX = width/2;
+  testCircleY = height/2;
   angle = 0.05;
-  scalar = 100;
-  speed = 0.05;
+  speed = 0.025;
 }
 
 function draw() {
-  //set ellipse placement variables
-  var x = constant + sin(angle) * scalar;
-  var y = constant + cos(angle) * scalar;
+  // Draw background to avoid shadows
+  var b = color('#000');
+  fill(b);
+  rect(0,0,width,height);
   
-  //create the test circle
-  var c = color('#0F0');
-  fill(c);
-  ellipse(x, y, 100, 100);
+  // Recreate the room to hide shadow of test circle
+  var c = color('#FFF'); //defines color 'c'
+  fill(c); //use color variable 'c' as fill color
+  ellipse(width/2, height/2, 600, 600);
+  
+  // set up ellipse placement vars for ellipse 1
+  var a = testCircleX + cos(angle) * 250;
+  var b = testCircleY + sin(angle) * 250;
+  
+  // create test circle 1
+  var g = color('#0A0');
+  fill(g);
+  ellipse(a, b, 100, 100);
+
   
   angle = angle + speed;
 }
