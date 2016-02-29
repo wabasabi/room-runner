5d23491776d7c253343a436b6aff4e723738a755
wabasabi <rhwabasabi@gmail.com>
2016-02-29 15:25:41 -0500

    added litterbug class

---
+++ room-runner/room-runner.js/Litterbug.js
@@ -0,0 +1,50 @@
+// Test sphere that moves around on its own
+function Litterbug() {
+  this.centerX = width / 2;
+  this.centerY = height / 2;
+  this.x = 0;
+  this.y = 0;
+  this.angle = 0;
+  this.speed = 0.017;
+  this.dimXY = height / 6;
+  this.scalar = height / 2 - (this.dimXY / 2);
+
+  this.isJumping = false;
+  this.isFalling = false;
+
+  this.draw = function() {
+
+    image(TT_Idle, this.x - 35, this.y - 45);
+
+    if (this.isJumping) {
+      this.scalar = this.scalar - 7;
+      if (this.scalar < height / 3) {
+        this.isJumping = false;
+        this.isFalling = true;
+      }
+    }
+
+    if (this.isFalling) {
+      this.scalar = this.scalar + 7;
+      this.isJumping = false;
+      if (this.scalar > height / 2 - (this.dimXY / 2)) {
+        this.isFalling = false;
+      }
+    }
+  }
+
+  this.moveRight = function() {
+    this.angle = this.angle - this.speed;
+    // Added to test animation
+    animation(walkingAnimation, this.x, this.y);
+  }
+
+  this.moveLeft = function() {
+    this.angle = this.angle + this.speed;
+    animation(walkingAnimation, this.x, this.y);
+  }
+
+  this.jump = function() {
+    this.isJumping = true;
+  }
+}
